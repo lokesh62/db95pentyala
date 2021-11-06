c3b94909ba12dc60fe602dc32750631a1653fccf
lokesh62 <lokeshpentyala1999@gmail.com>
2021-11-05 23:12:23 -0500

    modified

---
+++ routes/dist/selector.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+  res.render('selector', {
+    title: 'Selector'
+  });
+});
+module.exports = router;
+//# sourceMappingURL=selector.dev.js.map
