f95df99b3b0cd9787e10c4cfc814b653cf87ed04
lokesh62 <lokeshpentyala1999@gmail.com>
2021-11-17 16:37:20 -0600

    modified files

--- controllers/car.js
+++ controllers/car.js
@@ -89,6 +89,62 @@ exports.car_view_all_Page = async function(req, res) {
   }   
 }; 
 
+// Handle a show one view with id specified by query
+exports.costume_view_one_Page = async function(req, res) {
+console.log("single view for id " + req.query.id)
+try{
+result = await car.findById( req.query.id)
+res.render('car',
+{ title: 'car Detail', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.car_create_Page = function(req, res) {
+console.log("create view")
+try{
+res.render('carcreate', { title: 'car Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle a delete one view with id from query
+exports.costume_delete_Page = async function(req, res) {
+console.log("Delete view for id " + req.query.id)
+try{
+result = await Costume.findById(req.query.id)
+res.render('cardelete', { title: 'car Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.costume_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+let result = await Costume.findById(req.query.id)
+res.render('carupdate', { title: 'car Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
 exports.car_view_one_Page = async function(req, res) { 
   console.log("single view for id "  + req.query.id) 
   try{ 
@@ -117,7 +173,7 @@ exports.car_create_Page =  function(req, res) {
 }; 
 
 // Handle building the view for updating a costume. 
-// query provides the id s
+// query provides the id 
 exports.car_update_Page =  async function(req, res) { 
     console.log("update view for item "+req.query.id) 
     try{ 
--- routes/car.js
+++ routes/car.js
@@ -17,4 +17,13 @@ router.get('/update', car_controlers.car_update_Page);
 /* GET delete costume page */ 
 router.get('/delete', car_controlers.car_delete_Page);
 
+/* GET detail costume page */
+router.get('/detail', car_controlers.car_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', car_controlers.car_create_Page);
+
+/* GET create update page */
+/*router.get('/update, car_controlers.car_update_Page); */
+
 module.exports = router; 
